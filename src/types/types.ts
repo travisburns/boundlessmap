cd4daf29c2dc4de2842f4993dfa5cf5a949215e4
Travis Burns <travisjb0@gmail.com>
2024-07-08 17:52:41 -0700

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,23 +1,37 @@
 export interface Location {
-    Name: string;
-    id: string;
-    description: string;
-    XCoord: number;
-    YCoord: number;
-    image: string;
-    region: string;
-    type: string;
-    stats: {
-      population: number;
-      magic: number;
-      tech: number;
-      money: number;
-      governance: string;
-      military: string;
-      resource: string;
-      peaceLevel: string;
+  Name: string;
+  id: string;
+  description: string;
+  XCoord: number;
+  YCoord: number;
+  image: string;
+  region: string;
+  type: string;
+  stats: {
+    population: number;
+    magic: number;
+    tech: number;
+    money: number;
+    governance: string;
+    military: string;
+    resource: string;
+    peaceLevel: string;
+  };
+  geography: {
+    climate: string;
+    terrain: string[];
+    resources: string[];
+    landmarks: string[];
+    waterFeatures: string[];
+    vegetation: string[];
+    geographicalFormations: string[];
+    "flora and fauna": {
+      plants: string[];
+      animals: string[];
+      insects: string[];
     };
-    species: Record<string, number>;
-    classes: Record<string, number>;
-    faction: Record<string, number>;
-  }
+  };
+  species: Record<string, number>;
+  classes: Record<string, number>;
+  faction: Record<string, number>;
+}
