183394dcc1d97ae9b830df2b5c637513616d8d4e
Travis Burns <travisjb0@gmail.com>
2024-07-08 17:52:07 -0700

    Update DetailsTab.tsx

--- src/app/Components/DetailsTab.tsx
+++ src/app/Components/DetailsTab.tsx
@@ -1,47 +1,8 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-
+import { Location } from '../../types/types';
 import DonutChart from './DonutChart';
 import ClassesChart from './ClassesChart';
-
-interface Location {
-  Name: string;
-  id: string;
-  description: string;
-  XCoord: number;
-  YCoord: number;
-  image: string;
-  region: string;
-  type: string;
-  stats: {
-    population: number;
-    magic: number;
-    tech: number;
-    money: number;
-    governance: string;
-    military: string;
-    resource: string;
-    peaceLevel: string;
-  };
-  geography: {
-    climate: string;
-    terrain: string[];
-    resources: string[];
-    landmarks: string[];
-    waterFeatures: string[];
-    vegetation: string[];
-    geographicalFormations: string[];
-    "flora and fauna": {
-      plants: string[];
-      animals: string[];
-      insects: string[];
-    };
-  };
-  species: Record<string, number>;
-  classes: Record<string, number>;
-  faction: Record<string, number>;
-}
-
 
 
 interface DetailsTabProps {
